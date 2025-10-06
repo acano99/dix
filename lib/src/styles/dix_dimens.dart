94716a0c81445590c80a0a007ddfca8c82199f9d
acano99 <abrahan.jcano99@gmail.com>
2025-10-06 14:21:33 -0400

    feat: dimensiones bases comunmente usadas

---
+++ lib/src/styles/dix_dimens.dart
@@ -0,0 +1,8 @@
+class DixDimens {
+  static const double small = 8.0;
+  static const double normal = 12.0;
+  static const double medium = 16.0;
+  static const double hig = 20.0;
+  static const double large = 24.0;
+  static const double veryLarge = 28.0;
+}
