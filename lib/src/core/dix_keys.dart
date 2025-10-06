40df18563a772d3868c3ed0ce0589b94653b1389
acano99 <abrahan.jcano99@gmail.com>
2025-10-06 16:05:50 -0400

    doc: documentacion de la clase

--- lib/src/core/dix_keys.dart
+++ lib/src/core/dix_keys.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
+/// Esta clase establece claves globales para poder usar el contexto
+/// en cualquier parte de la aplicación.
 class DixKeys {
+  /// Clave global para acceder al estado del Navigator desde cualquier parte.
   static final GlobalKey<NavigatorState> navigatorKey =
       GlobalKey<NavigatorState>();
 
+  /// Clave global para acceder al ScaffoldMessenger y mostrar mensajes/snackbars.
   static final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
       GlobalKey<ScaffoldMessengerState>();
 }
