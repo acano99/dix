8965895098c27a81d3eef69d596b9b75bdd3bb31
acano99 <abrahan.jcano99@gmail.com>
2025-10-06 14:20:21 -0400

    feat: claves globales y punto de entrada de la aplicacion para la inyeccion del contexto de forma glogal

---
+++ lib/src/core/dix_keys.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class DixKeys {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey =
+      GlobalKey<ScaffoldMessengerState>();
+}
---
+++ lib/src/core/dix_material_app.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+import 'dix_keys.dart';
+
+class DixMaterialApp extends StatefulWidget {
+  final GlobalKey<NavigatorState>? navigatorKey;
+  final GlobalKey<ScaffoldMessengerState>? scaffoldMessengerKey;
+
+  // Todas las props de MaterialApp
+  final Widget? home;
+  final Map<String, WidgetBuilder> routes;
+  final String? initialRoute;
+  final RouteFactory? onGenerateRoute;
+  final InitialRouteListFactory? onGenerateInitialRoutes;
+  final RouteFactory? onUnknownRoute;
+  final List<NavigatorObserver> navigatorObservers;
+  final TransitionBuilder? builder;
+  final String title;
+  final GenerateAppTitle? onGenerateTitle;
+  final Color? color;
+  final ThemeData? theme;
+  final ThemeData? darkTheme;
+  final ThemeMode themeMode;
+  final TextDirection? textDirection;
+  final Locale? locale;
+  final Iterable<LocalizationsDelegate<dynamic>>? localizationsDelegates;
+  final LocaleListResolutionCallback? localeListResolutionCallback;
+  final LocaleResolutionCallback? localeResolutionCallback;
+  final Iterable<Locale> supportedLocales;
+  final bool debugShowMaterialGrid;
+  final bool showPerformanceOverlay;
+  final bool checkerboardRasterCacheImages;
+  final bool checkerboardOffscreenLayers;
+  final bool showSemanticsDebugger;
+  final bool debugShowCheckedModeBanner;
+  final Map<LogicalKeySet, Intent>? shortcuts;
+  final ScrollBehavior? scrollBehavior;
+  final ThemeData? highContrastTheme;
+  final ThemeData? highContrastDarkTheme;
+  final Map<Type, Action<Intent>>? actions;
+  final bool useInheritedMediaQuery;
+
+  // Router mode
+  final RouteInformationProvider? routeInformationProvider;
+  final RouteInformationParser<Object>? routeInformationParser;
+  final RouterDelegate<Object>? routerDelegate;
+  final RouterConfig<Object>? routerConfig;
+  final BackButtonDispatcher? backButtonDispatcher;
+
+  const DixMaterialApp({
+    super.key,
+    this.navigatorKey,
+    this.scaffoldMessengerKey,
+    this.home,
+    this.routes = const <String, WidgetBuilder>{},
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onGenerateInitialRoutes,
+    this.onUnknownRoute,
+    this.navigatorObservers = const <NavigatorObserver>[],
+    this.builder,
+    this.title = '',
+    this.onGenerateTitle,
+    this.color,
+    this.theme,
+    this.darkTheme,
+    this.themeMode = ThemeMode.system,
+    this.textDirection,
+    this.locale,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales = const <Locale>[Locale('en', 'US')],
+    this.debugShowMaterialGrid = false,
+    this.showPerformanceOverlay = false,
+    this.checkerboardRasterCacheImages = false,
+    this.checkerboardOffscreenLayers = false,
+    this.showSemanticsDebugger = false,
+    this.debugShowCheckedModeBanner = true,
+    this.shortcuts,
+    this.scrollBehavior,
+    this.highContrastTheme,
+    this.highContrastDarkTheme,
+    this.actions,
+    this.useInheritedMediaQuery = false,
+  }) : routeInformationProvider = null,
+       routeInformationParser = null,
+       routerDelegate = null,
+       routerConfig = null,
+       backButtonDispatcher = null;
+
+  const DixMaterialApp.router({
+    super.key,
+    this.scaffoldMessengerKey,
+    this.routeInformationProvider,
+    this.routeInformationParser,
+    this.routerDelegate,
+    this.routerConfig,
+    this.backButtonDispatcher,
+    this.builder,
+    this.title = '',
+    this.onGenerateTitle,
+    this.color,
+    this.theme,
+    this.darkTheme,
+    this.useInheritedMediaQuery = false,
+    this.highContrastTheme,
+    this.highContrastDarkTheme,
+    this.themeMode = ThemeMode.system,
+    this.locale,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales = const <Locale>[Locale('en', 'US')],
+    this.debugShowMaterialGrid = false,
+    this.showPerformanceOverlay = false,
+    this.checkerboardRasterCacheImages = false,
+    this.checkerboardOffscreenLayers = false,
+    this.showSemanticsDebugger = false,
+    this.debugShowCheckedModeBanner = true,
+    this.shortcuts,
+    this.scrollBehavior,
+    this.actions,
+    this.textDirection,
+  }) : navigatorKey = null,
+       home = null,
+       routes = const {},
+       initialRoute = null,
+       onGenerateRoute = null,
+       onGenerateInitialRoutes = null,
+       onUnknownRoute = null,
+       navigatorObservers = const [];
+
+  @override
+  State<DixMaterialApp> createState() => _DixMaterialAppState();
+}
+
+class _DixMaterialAppState extends State<DixMaterialApp> {
+  @override
+  Widget build(BuildContext context) {
+    if (widget.routerDelegate != null || widget.routerConfig != null) {
+      // Modo Router (GoRouter, Router API, etc.)
+      return MaterialApp.router(
+        routerDelegate: widget.routerDelegate,
+        routeInformationParser: widget.routeInformationParser,
+        routeInformationProvider: widget.routeInformationProvider,
+        routerConfig: widget.routerConfig,
+        backButtonDispatcher: widget.backButtonDispatcher,
+        builder:
+            widget.builder ??
+            (context, child) {
+              // Mantener DixKeys.scaffoldMessengerKey si quieres
+              return ScaffoldMessenger(
+                key: DixKeys.scaffoldMessengerKey,
+                child: child ?? const SizedBox.shrink(),
+              );
+            },
+        title: widget.title,
+        onGenerateTitle: widget.onGenerateTitle,
+        color: widget.color,
+        theme: widget.theme,
+        darkTheme: widget.darkTheme,
+        themeMode: widget.themeMode,
+        localizationsDelegates: widget.localizationsDelegates,
+        localeListResolutionCallback: widget.localeListResolutionCallback,
+        localeResolutionCallback: widget.localeResolutionCallback,
+        supportedLocales: widget.supportedLocales,
+        debugShowMaterialGrid: widget.debugShowMaterialGrid,
+        showPerformanceOverlay: widget.showPerformanceOverlay,
+        checkerboardRasterCacheImages: widget.checkerboardRasterCacheImages,
+        checkerboardOffscreenLayers: widget.checkerboardOffscreenLayers,
+        showSemanticsDebugger: widget.showSemanticsDebugger,
+        debugShowCheckedModeBanner: widget.debugShowCheckedModeBanner,
+        shortcuts: widget.shortcuts,
+        scrollBehavior: widget.scrollBehavior,
+        highContrastTheme: widget.highContrastTheme,
+        highContrastDarkTheme: widget.highContrastDarkTheme,
+        actions: widget.actions,
+      );
+    }
+
+    // Modo clásico
+    return MaterialApp(
+      navigatorKey: widget.navigatorKey ?? DixKeys.navigatorKey,
+      scaffoldMessengerKey:
+          widget.scaffoldMessengerKey ?? DixKeys.scaffoldMessengerKey,
+      home: widget.home,
+      routes: widget.routes,
+      initialRoute: widget.initialRoute,
+      onGenerateRoute: widget.onGenerateRoute,
+      onGenerateInitialRoutes: widget.onGenerateInitialRoutes,
+      onUnknownRoute: widget.onUnknownRoute,
+      navigatorObservers: widget.navigatorObservers,
+      builder: widget.builder,
+      title: widget.title,
+      onGenerateTitle: widget.onGenerateTitle,
+      color: widget.color,
+      theme: widget.theme,
+      darkTheme: widget.darkTheme,
+      themeMode: widget.themeMode,
+      locale: widget.locale,
+      localizationsDelegates: widget.localizationsDelegates,
+      localeListResolutionCallback: widget.localeListResolutionCallback,
+      localeResolutionCallback: widget.localeResolutionCallback,
+      supportedLocales: widget.supportedLocales,
+      debugShowMaterialGrid: widget.debugShowMaterialGrid,
+      showPerformanceOverlay: widget.showPerformanceOverlay,
+      checkerboardRasterCacheImages: widget.checkerboardRasterCacheImages,
+      checkerboardOffscreenLayers: widget.checkerboardOffscreenLayers,
+      showSemanticsDebugger: widget.showSemanticsDebugger,
+      debugShowCheckedModeBanner: widget.debugShowCheckedModeBanner,
+      shortcuts: widget.shortcuts,
+      scrollBehavior: widget.scrollBehavior,
+      highContrastTheme: widget.highContrastTheme,
+      highContrastDarkTheme: widget.highContrastDarkTheme,
+      actions: widget.actions,
+    );
+  }
+}
