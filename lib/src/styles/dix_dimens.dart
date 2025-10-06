33802f7614413424c45b9e560a49c0eeb5d3577d
acano99 <abrahan.jcano99@gmail.com>
2025-10-06 16:25:40 -0400

    doc: documentacion de la clase

--- lib/src/styles/dix_dimens.dart
+++ lib/src/styles/dix_dimens.dart
@@ -1,8 +1,21 @@
+/// Clase con dimensiones comunmente usadas en el desarrollo
+/// de aplicaciones.
 class DixDimens {
+  /// Dimensión pequeña, típicamente usada para espaciados o tamaños mínimos.
   static const double small = 8.0;
+
+  /// Dimensión normal, un tamaño estándar para muchos elementos de UI.
   static const double normal = 12.0;
+
+  /// Dimensión media, ligeramente más grande que la normal.
   static const double medium = 16.0;
+
+  /// Dimensión alta, para elementos que requieren más espacio.
   static const double hig = 20.0;
+
+  /// Dimensión grande, para elementos prominentes o espaciados generosos.
   static const double large = 24.0;
+
+  /// Dimensión muy grande, para elementos que necesitan destacar mucho.
   static const double veryLarge = 28.0;
 }
